62953b9a48bffb2401c53360c85482ee09945ddd
kim866 <45016938+kim866@users.noreply.github.com>
2019-02-13 11:37:24 -0500

    Add files via upload

--- Advanced_Data_Storage_and_Retrieval HW/climate_app.py
+++ Advanced_Data_Storage_and_Retrieval HW/climate_app.py
@@ -14,7 +14,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, and_
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from dateutil.relativedelta import relativedelta
 
 engine = create_engine("sqlite:///hawaii.sqlite")
@@ -28,14 +28,15 @@ Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 
 # Create our session (link) from Python to the DB
-session = Session(engine)
 
 app = Flask(__name__)
 
 @app.route("/")
+
 def home():
     print("Server received request for 'Home' page...")
     
+    session = Session(engine)
     query_dates = session.query(Measurement.date).all()
     dates = [date[0] for date in query_dates]
     last_date = max(dates)
@@ -51,8 +52,10 @@ def home():
         )
 
 @app.route("/api/v1.0/stations")
+
 def stations():
     print("Server received request for 'Stations' page...")
+    session = Session(engine)
     results = session.query(Measurement.station).distinct(Measurement.station).all()
 
     # Convert list of tuples into normal list
@@ -61,8 +64,10 @@ def stations():
     return jsonify(all_names)
 
 @app.route("/api/v1.0/tobs")
+
 def tobs():
     print("Server received request for 'Tobs' page...")
+    session = Session(engine)
 
     query_dates = session.query(Measurement.date).all()
     dates = [date[0] for date in query_dates]
@@ -84,33 +89,10 @@ def tobs():
     return jsonify(last_one_year_temp_list)
 
 
-@app.route("/api/v1.0/<start_date>/<end_date>")
-def start_and_end_dates(start_date, end_date):
-    print("Server received request for 'Start_Date & End_Date' page...")
-    
-    
-    results = session.query(Measurement.date)
-    date_list = [x[0] for x in results]
-    
-    if start_date in date_list and end_date in date_list:
-
-    
-        lowest_temp = session.query(func.min(Measurement.tobs)).filter(and_(Measurement.date >= start_date, Measurement.date <= end_date)).all()
-        highest_temp = session.query(func.max(Measurement.tobs)).filter(and_(Measurement.date >= start_date, Measurement.date <= end_date)).all()
-        avg_temp = session.query(func.avg(Measurement.tobs)).filter(and_(Measurement.date >= start_date, Measurement.date <= end_date)).all()
-        
-        return (
-            f"The lowest temp is {[x[0] for x in lowest_temp]}.<br/>"
-            f"The highest temp is {[x[0] for x in highest_temp]}.<br/>"
-            f"The average temp is {[x[0] for x in avg_temp]}<br/>"
-            )
-    
-    if start_date not in date_list or end_date not in date_list:
-        return jsonify({"error": f"Date {start_date} or {end_date} out of range. Please pick another dates."}), 404
-
-
 @app.route("/api/v1.0/<start_date>")
+
 def start_date(start_date):
+    session = Session(engine)
     print("Server received request for 'Start_Date' page...")
     
     input_date = start_date
@@ -134,6 +116,35 @@ def start_date(start_date):
     if input_date not in date_list:
         return jsonify({"error": f"Date {start_date} out of range. Please pick another date."}), 404
 
+
+@app.route("/api/v1.0/<start_date>/<end_date>")
+
+def start_and_end_dates(start_date, end_date):
+    session = Session(engine)
+    print("Server received request for 'Start_Date & End_Date' page...")
+    
+    
+    results = session.query(Measurement.date)
+    date_list = [x[0] for x in results]
+    
+    if start_date in date_list and end_date in date_list:
+
+    
+        lowest_temp = session.query(func.min(Measurement.tobs)).filter(and_(Measurement.date >= start_date, Measurement.date <= end_date)).all()
+        highest_temp = session.query(func.max(Measurement.tobs)).filter(and_(Measurement.date >= start_date, Measurement.date <= end_date)).all()
+        avg_temp = session.query(func.avg(Measurement.tobs)).filter(and_(Measurement.date >= start_date, Measurement.date <= end_date)).all()
+        
+        return (
+            f"The lowest temp is {[x[0] for x in lowest_temp]}.<br/>"
+            f"The highest temp is {[x[0] for x in highest_temp]}.<br/>"
+            f"The average temp is {[x[0] for x in avg_temp]}<br/>"
+            )
+    
+    if start_date not in date_list or end_date not in date_list:
+        return jsonify({"error": f"Date {start_date} or {end_date} out of range. Please pick another dates."}), 404
+
+
+from flask import request
 
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
